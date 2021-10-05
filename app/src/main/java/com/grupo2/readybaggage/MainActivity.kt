413dfe3c2f2822742dad47bf12277ca68516c120
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-10-05 13:59:09 +0200

    Función de cambiar idioma

--- app/src/main/java/com/grupo2/readybaggage/MainActivity.kt
+++ app/src/main/java/com/grupo2/readybaggage/MainActivity.kt
@@ -1,30 +1,51 @@
 package com.grupo2.readybaggage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var locale: Locale
+    private var currentLang: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    //insertar menú
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_idioma, menu)
         return true
     }
-
+    //items de menú clickables
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val iditem = item.getItemId()
-        if (iditem==R.id.spanish){
-
-        }
-        if (iditem==R.id.euskera){
-
+        when (item.itemId) {
+            R.id.spanish -> setLocale("es")
+            R.id.euskera -> setLocale("eu")
+            R.id.english -> setLocale("en")
         }
         return true
+    }
+
+    //cambiar idioma
+    private fun setLocale(localeName: String) {
+        locale = Locale(localeName)
+        val res = resources
+        val dm = res.displayMetrics
+        val conf = res.configuration
+        conf.locale = locale
+        res.updateConfiguration(conf, dm)
+        val refresh = Intent(
+            this,
+            this::class.java
+        )
+        refresh.putExtra(currentLang, localeName)
+        startActivity(refresh)
     }
 }
