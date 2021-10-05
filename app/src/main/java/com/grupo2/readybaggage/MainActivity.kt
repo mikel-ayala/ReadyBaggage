34cddf06853c3034c5ccc8b513cbcc43b773378d
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-10-05 11:58:34 +0200

    menu idiomas

--- app/src/main/java/com/grupo2/readybaggage/MainActivity.kt
+++ app/src/main/java/com/grupo2/readybaggage/MainActivity.kt
@@ -2,10 +2,29 @@ package com.grupo2.readybaggage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_idioma, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val iditem = item.getItemId()
+        if (iditem==R.id.spanish){
+
+        }
+        if (iditem==R.id.euskera){
+
+        }
+        return true
     }
 }
