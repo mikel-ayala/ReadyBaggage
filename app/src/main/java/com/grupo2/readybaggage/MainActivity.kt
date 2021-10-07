bb1863ee45da0ee2e0f45e3ab651e5570d985f58
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-10-07 10:16:12 +0200

    Botón de idiomas

--- app/src/main/java/com/grupo2/readybaggage/MainActivity.kt
+++ app/src/main/java/com/grupo2/readybaggage/MainActivity.kt
@@ -1,51 +1,76 @@
 package com.grupo2.readybaggage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.MenuInflater
+import android.view.View
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
+
     lateinit var locale: Locale
+    private var currentLanguage = Locale.getDefault()
     private var currentLang: String? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val idiomas=getSharedPreferences("idiomas", 0)
+        if(idiomas.getString("idioma", "").isNullOrEmpty()){
+            currentLang = currentLanguage.toString()
+        }else{
+            currentLang = idiomas.getString("idioma", "")
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-
-    //insertar menú
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_idioma, menu)
-        return true
-    }
-    //items de menú clickables
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.spanish -> setLocale("es")
-            R.id.euskera -> setLocale("eu")
-            R.id.english -> setLocale("en")
+        btIdiomas.setOnClickListener{
+            showPopup(btIdiomas)
         }
-        return true
+
+    }
+
+
+    fun showPopup(v : View){
+        val popup = PopupMenu(this, v)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.menu_idioma, popup.menu)
+        popup.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.spanish-> {
+//                    idiomas.edit().putString("idioma", "es")
+                    setLocale("es")
+                }
+                R.id.euskera-> {
+//                    idiomas.edit().putString("idioma", "eu")
+                    setLocale("eu")
+                }
+                R.id.english-> {
+//                    idiomas.edit().putString("idioma", "en")
+                    setLocale("en")
+                }
+            }
+            true
+        }
+        popup.show()
     }
 
     //cambiar idioma
     private fun setLocale(localeName: String) {
-        locale = Locale(localeName)
-        val res = resources
-        val dm = res.displayMetrics
-        val conf = res.configuration
-        conf.locale = locale
-        res.updateConfiguration(conf, dm)
-        val refresh = Intent(
-            this,
-            this::class.java
-        )
-        refresh.putExtra(currentLang, localeName)
-        startActivity(refresh)
+            locale = Locale(localeName)
+            val res = resources
+            val dm = res.displayMetrics
+            val conf = res.configuration
+            conf.locale = locale
+            res.updateConfiguration(conf, dm)
+            val refresh = Intent(
+                this,
+                MainActivity::class.java
+            )
+            refresh.putExtra(currentLang, localeName)
+            startActivity(refresh)
     }
 }
