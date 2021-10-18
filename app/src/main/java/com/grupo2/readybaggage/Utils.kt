316c44df8f281f1f1da8fbcc51b2f838301f1d62
mikel-ayala <75023127+mikel-ayala@users.noreply.github.com>
2021-10-18 08:26:34 +0200

    Update Utils.kt

--- app/src/main/java/com/grupo2/readybaggage/Utils.kt
+++ app/src/main/java/com/grupo2/readybaggage/Utils.kt
@@ -1,11 +1,21 @@
 package com.grupo2.readybaggage
 
+import android.content.Intent
 import android.text.TextUtils
-import kotlinx.android.synthetic.main.activity_register.*
+import android.view.MenuInflater
+import android.view.View
+import android.widget.PopupMenu
+import kotlinx.android.synthetic.main.*
 import java.lang.Exception
+import java.util.*
 
 class Utils {
     companion object {
+
+        private lateinit var locale: Locale
+        private var currentLanguage = Locale.getDefault()
+        private lateinit var currentLang: String
+
         //Función para comprobar que el formato del nº de teléfono es correcto
         fun validarTelefono(pTelefono: String): Boolean {
             try {
@@ -34,6 +44,49 @@ class Utils {
                 return true
             }
             return false
+        }
+
+        //Desplegar menú
+        private fun showPopup(v : View){
+            val popup = PopupMenu(this, v)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.menu_idioma, popup.menu)
+            //Controlar click en los items
+            popup.setOnMenuItemClickListener { menuItem ->
+                when(menuItem.itemId){
+                    R.id.spanish-> {
+//                    SharedApp.prefs.dato="es"
+                        setLocale("es")
+                    }
+                    R.id.euskera-> {
+//                    SharedApp.prefs.dato="eu"
+                        setLocale("eu")
+                    }
+                    R.id.english-> {
+//                    SharedApp.prefs.dato="en"
+                        setLocale("en")
+                    }
+                }
+                true
+            }
+            popup.show()
+        }
+
+        //cambiar idioma
+        private fun setLocale(localeName: String) {
+            locale = Locale(localeName)
+            val res = resources
+            val dm = res.displayMetrics
+            val conf = res.configuration
+            conf.locale = locale
+            res.updateConfiguration(conf, dm)
+            val refresh = Intent(
+                this,
+                this::class.java
+            )
+            refresh.putExtra(currentLang, localeName)
+            finish()
+            startActivity(refresh)
         }
     }
 }
