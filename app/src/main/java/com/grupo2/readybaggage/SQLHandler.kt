385a1eec91ee98fd118a845aba2be623441054ea
mikel-ayala <75023127+mikel-ayala@users.noreply.github.com>
2021-10-22 10:22:44 +0200

    tema

--- app/src/main/java/com/grupo2/readybaggage/SQLHandler.kt
+++ app/src/main/java/com/grupo2/readybaggage/SQLHandler.kt
@@ -1,5 +1,6 @@
 package com.grupo2.readybaggage
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -12,6 +13,14 @@ class SQLHandler(context: Context, name: String, factory: SQLiteDatabase.CursorF
         db.execSQL("create table cliente(idCliente real PRIMARY KEY AUTOINCREMENT, email text UNIQUE NOT NULL, pass text not null, nombre texT not null, apellidos text, telefono text not null, f_registro text DEFAULT CURRENT_DATE, is_empleado real DEFAULT 0)")
         //db.execSQL("create table producto(idProducto real PRIMARY KEY AUTOINCREMENT, nombre text not null, descripcion text not null, precio real not null)")
         //db.execSQL("create table reserva(idReserva real PRIMARY KEY AUTOINCREMENT, idCliente real not null, idProducto real not null, cantidadProducto real not null, fSolicitud text not null, origen text not null, destino text not null, fRecogida text not null, fEntrega text not null, hRecogida text not null, hEntrega text not null, ubicacion text not null, FOREIGN KEY(idCliente) REFERENCES cliente(idCliente), FOREIGN KEY(idProducto) REFERENCES producto(idProducto))")
+
+        val row = ContentValues()
+        row.put("email", "admin@gmail.com")
+        row.put("pass", "Admin_1234")
+        row.put("nombre", "Administrador")
+        row.put("apellidos", "Oficial")
+        row.put("telefono", "123456789")
+        db.insert("cliente", null, row)
 
     }
 
