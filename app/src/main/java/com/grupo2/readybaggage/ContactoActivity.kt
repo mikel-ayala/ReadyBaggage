60be9a1cfb5a8b8c26bc376dd1b0a24448efc791
GonzaloAlonso95 <gonzalo.alonso@elorrieta-errekamari.com>
2021-10-28 11:54:01 +0200

    Update enlaces contacto

--- app/src/main/java/com/grupo2/readybaggage/ContactoActivity.kt
+++ app/src/main/java/com/grupo2/readybaggage/ContactoActivity.kt
@@ -1,8 +1,11 @@
 package com.grupo2.readybaggage
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.grupo2.readybaggage.Utils.Companion.startActivity
+import kotlinx.android.synthetic.main.activity_contacto.*
 import kotlinx.android.synthetic.main.menu_inferior.*
 import kotlinx.android.synthetic.main.menu_superior.*
 
@@ -18,7 +21,36 @@ class ContactoActivity : AppCompatActivity() {
             Utils.showPopup(btIdiomas, this, this)
         }//onClick
 
+        //Ir al main
+        iconoMain.setOnClickListener {
+            startActivity<MainActivity>()
+        }//onClick
 
+
+        //Icono Facebook
+        iconoFB.setOnClickListener{
+            abrirweb("https://www.facebook.com/")
+        }
+
+        //Icono Twitter
+        iconoTwitter.setOnClickListener{
+            abrirweb("https://twitter.com")
+        }
+
+        //Icono Instagram
+        iconoInsta.setOnClickListener{
+            abrirweb("https://www.instagram.com/")
+        }
+
+        //Icono LinkedIn
+        iconoLinkedIn.setOnClickListener{
+            abrirweb("https://www.linkedin.com/")
+        }
+
+        //Icono Whatsapp
+        iconoWhatsapp.setOnClickListener{
+
+        }
 
         //Ir al perfil
         iconoPerfil.setOnClickListener {
@@ -26,14 +58,14 @@ class ContactoActivity : AppCompatActivity() {
 
         }//onClick
 
-        //Ir al main
-        iconoMain.setOnClickListener {
-            startActivity<MainActivity>()
-        }//onClick
-
         //Ir a las reservas
         iconoReservas.setOnClickListener{
             startActivity<ReservasActivity>()
         }//onClick
+    }
+
+    fun abrirweb(web:String){
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(web))
+        startActivity(browserIntent)
     }
 }
